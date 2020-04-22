c93fb81666ff8086079fb3af01c4831311d8821e
zorluozan <61868614+zorluozan@users.noreply.github.com>
2020-04-22 23:07:13 +0300

    Version 1

---
+++ src/CrimeFile.java
@@ -0,0 +1,63 @@
+
+public class CrimeFile {
+
+	private String userName;
+    private String userAddress;
+    private String userPhone;
+    private String complaintType;
+	private String complaintDescription;
+    private String crimeTime;
+    private String crimeDate;
+    private String crimeLocation;
+    
+	public String getUserName() {
+		return userName;
+	}
+	public String getUserAddress() {
+		return userAddress;
+	}
+	public String getUserPhone() {
+		return userPhone;
+	}
+	public String getComplaintType() {
+		return complaintType;
+	}
+	public String getComplaintDescription() {
+		return complaintDescription;
+	}
+	public String getCrimeTime() {
+		return crimeTime;
+	}
+	public String getCrimeDate() {
+		return crimeDate;
+	}
+	public String getCrimeLocation() {
+		return crimeLocation;
+	}
+	
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public void setUserAddress(String userAddress) {
+		this.userAddress = userAddress;
+	}
+	public void setUserPhone(String userPhone) {
+		this.userPhone = userPhone;
+	}
+	public void setComplaintType(String complaintType) {
+		this.complaintType = complaintType;
+	}
+	public void setComplaintDescription(String complaintDescription) {
+		this.complaintDescription = complaintDescription;
+	}
+	public void setCrimeTime(String crimeTime) {
+		this.crimeTime = crimeTime;
+	}
+	public void setCrimeDate(String crimeDate) {
+		this.crimeDate = crimeDate;
+	}
+	public void setCrimeLocation(String crimeLocation) {
+		this.crimeLocation = crimeLocation;
+	}
+    
+}
---
+++ src/Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    	
+        Scanner input = new Scanner(System.in);
+        UserEntrence ue = new UserEntrence();
+        AdminEntrence ae = new AdminEntrence();
+        System.out.println("Welcome to the Crime File Management System!");     
+        while (true) {
+             displayMenu();
+             System.out.print("Please select your choice: ");
+             int selection = input.nextInt();
+             switch (selection) {
+                case 0:
+                    System.out.println("Have a good day, system terminated...");
+                    System.exit(0);
+                case 1:
+                	if(ue.userLogin()) { // Check the user information according to the database 
+                		ue.userdisplay(); //Display the user menu
+                        System.out.print("\nPlease select your option: ");
+                        int option = input.nextInt();
+                        switch(option) {
+                        case 1:
+                        	CrimeFile cfile = new CrimeFile();
+                        	ue.addfile(cfile);
+                        	break;
+                        }
+                	}
+                	else {
+                		System.out.println("\nAccess Denied");
+                	}
+                    break;
+                case 2:
+                    ae.adminLogin(); // You can type police ID as 12345 adminPass as admin to login
+                    break;
+                case 3:
+                    ue.userRegister();
+                    break;
+                default:
+                    System.out.println("Invalid number entered!");
+            }
+        }
+    }
+    //This is the main menu
+    public static void displayMenu() {
+    	System.out.println("\nPress 0 for exit");
+    	System.out.println("Press 1 for user login");
+    	System.out.println("Press 2 for admin login");
+    	System.out.println("Press 3 for user register");
+    }
+}
